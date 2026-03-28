2862af2b3a785c98d61632b973a27fc8433aab20
dilshan-cs <kavishka.dilshan.cs@gmail.com>
2026-03-28 21:35:01 +0530

    FireBase Configuration

---
+++ config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1MOp2UolylrRwSJhVM1ECBvhBKmrdVPc",
+  authDomain: "cricket-scoring-data.firebaseapp.com",
+  databaseURL: "https://cricket-scoring-data-default-rtdb.firebaseio.com",
+  projectId: "cricket-scoring-data",
+  storageBucket: "cricket-scoring-data.firebasestorage.app",
+  messagingSenderId: "1069597107626",
+  appId: "1:1069597107626:web:5e540bca96b6c0951ab7d3",
+  measurementId: "G-C59179B1ZJ"
+};
