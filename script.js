9e65e38dfc317326e079310438278f3ad55787c7
dilshan-cs <kavishka.dilshan.cs@gmail.com>
2026-03-28 21:05:47 +0530

    keyHidding

--- script.js
+++ script.js
@@ -2,17 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA1MOp2UolylrRwSJhVM1ECBvhBKmrdVPc",
-  authDomain: "cricket-scoring-data.firebaseapp.com",
-  databaseURL: "https://cricket-scoring-data-default-rtdb.firebaseio.com",
-  projectId: "cricket-scoring-data",
-  storageBucket: "cricket-scoring-data.firebasestorage.app",
-  messagingSenderId: "1069597107626",
-  appId: "1:1069597107626:web:5e540bca96b6c0951ab7d3",
-  measurementId: "G-C59179B1ZJ"
-};
+
+import{firebaseConfig} from "./config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
